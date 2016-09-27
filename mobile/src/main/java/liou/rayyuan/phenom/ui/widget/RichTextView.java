a1483258fa98f67075adb0bbb12d7f93a97b16d2
Ray <louis383@gmail.com>
2016-09-27 11:11:12 +0800

    RichTextView to display HTML img tag

--- mobile/src/main/java/liou/rayyuan/phenom/ui/widget/RichTextView.java
+++ mobile/src/main/java/liou/rayyuan/phenom/ui/widget/RichTextView.java
@@ -14,8 +14,6 @@ import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
-import liou.rayyuan.phenom.model.domain.Plurk;
-
 /**
  * Created by louis383 on 2016/6/25.
  */
@@ -23,27 +21,35 @@ import liou.rayyuan.phenom.model.domain.Plurk;
 public class RichTextView extends TextView {
 
     private Context context;
-    private Plurk plurk;
 
     public RichTextView(Context context) {
         super(context);
+        this.context = context;
     }
 
     public RichTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
     }
 
     public RichTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
     }
 
     public RichTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-
+        this.context = context;
     }
 
-    private void parseHTMLToRichText() {
-        Spanned textSpanned = Html.fromHtml(plurk.getContent(), new Html.ImageGetter() {
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        Spanned spannedText = parseHTMLToRichText(text);
+        super.setText(spannedText, type);
+    }
+
+    private Spanned parseHTMLToRichText(CharSequence text) {
+        Spanned textSpanned = Html.fromHtml(text.toString(), new Html.ImageGetter() {
             @Override
             public Drawable getDrawable(String source) {
                 if (TextUtils.isEmpty(source)) {
@@ -66,5 +72,7 @@ public class RichTextView extends TextView {
                 return drawable;
             }
         }, null);
+
+        return textSpanned;
     }
 }
--- mobile/src/main/java/liou/rayyuan/phenom/viewmodel/PlurkViewModel.java
+++ mobile/src/main/java/liou/rayyuan/phenom/viewmodel/PlurkViewModel.java
@@ -2,18 +2,6 @@ package liou.rayyuan.phenom.viewmodel;
 
 import android.content.Context;
 import android.databinding.BaseObservable;
-import android.databinding.Observable;
-import android.databinding.ObservableField;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.text.Html;
-import android.text.Spanned;
-import android.text.TextUtils;
-
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.interfaces.DraweeController;
-import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
 import java.util.Calendar;
 import java.util.Locale;
@@ -48,44 +36,8 @@ public class PlurkViewModel extends BaseObservable {
         return String.valueOf(plurk.getResponseCount());
     }
 
-    public Spanned getMessage() {
-        if (context == null) {
-            return null;
-        }
-
-        Spanned textSpanned = Html.fromHtml(plurk.getContent(), new Html.ImageGetter() {
-            @Override
-            public Drawable getDrawable(String source) {
-                if (TextUtils.isEmpty(source)) {
-                   return null;
-                }
-
-                SimpleDraweeView simpleDraweeView = new SimpleDraweeView(context);
-                ImageRequestBuilder requestBuilder = ImageRequestBuilder.newBuilderWithSource(Uri.parse(source));
-                DraweeController controller = Fresco.newDraweeControllerBuilder()
-                        .setImageRequest(requestBuilder.build())
-                        .setOldController(simpleDraweeView.getController())
-                        .setAutoPlayAnimations(true)
-                        .build();
-
-                simpleDraweeView.setController(controller);
-
-                Drawable drawable = simpleDraweeView.getTopLevelDrawable();
-                drawable.setBounds(0, 0, 100, 100);
-
-                return drawable;
-            }
-        }, null);
-
-        ObservableField<Spanned> observableSpanned = new ObservableField<>(textSpanned);
-        observableSpanned.addOnPropertyChangedCallback(new OnPropertyChangedCallback() {
-            @Override
-            public void onPropertyChanged(Observable observable, int i) {
-                notifyPropertyChanged(liou.rayyuan.phenom.BR.plurk);
-            }
-        });
-
-        return observableSpanned.get();
+    public String getMessage() {
+        return plurk.getContent();
     }
 
     public String getPlurkId() {
